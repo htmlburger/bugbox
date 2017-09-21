7ec9957016c5980610d23e781486c32e0ef92655
Yuliyan Slavchev <yuliyan.slavchev@gmail.com>
2017-09-21 14:06:00 +0300

    Update pin style

--- src/components/Pins.vue
+++ src/components/Pins.vue
@@ -39,16 +39,16 @@ export default {
 
 	@keyframes bugboxScale {
 		0% { transform: scale(1); opacity: 1; }
-		50% { transform: scale(1.25); opacity: .8; }
+		50% { transform: scale(1.25); opacity: .5; }
 		100% { transform: scale(1); opacity: 1; }
 	}
 
-	.bugbox .bugbox__pin { position: absolute; left: -10000px; top: -10000px; margin: -41px 0 0 -15px; cursor: pointer; transition: opacity .2s; }
+	.bugbox .bugbox__pin { position: absolute; left: -10000px; top: -10000px; margin: -27px 0 0 -10px; cursor: pointer; transition: opacity .2s; }
 
-	.bugbox .bugbox__pin:after { content: ''; position: relative; display: block; width: 30px; height: 44px; background: url(~assets/images/pin-default.svg) no-repeat center center; background-size: 100% 100%; transition: all .2s; }
-	.bugbox .bugbox__pin:before { content: ''; position: absolute; left: 50%; top: 50%; width: 56px; height: 56px; margin: -28px 0 0 -28px; background: #f05d5d; border-radius: 50%; visibility: hidden; opacity: 0; transform: scale(.5); transition: all .2s; }
+	.bugbox .bugbox__pin:after { content: ''; position: relative; display: block; width: 20px; height: 28px; background: url(~assets/images/pin-default.svg) no-repeat center center; background-size: 100% 100%; transition: all .2s; }
+	.bugbox .bugbox__pin:before { content: ''; position: absolute; left: 50%; top: 50%; width: 28px; height: 28px; margin: -15px 0 0 -14px; background: #f05d5d; border-radius: 50%; visibility: hidden; opacity: 0; transform: scale(.5); transition: all .2s; }
 
-	.bugbox .bugbox__pin:hover:after { transform: translateY(-5px); }
+	.bugbox .bugbox__pin:hover:after { transform: translateY(-3px); }
 	.bugbox .bugbox__pin:active:after { transform: translateY(0); }
 
 	.bugbox .bugbox__pin--done { visibility: hidden; opacity: 0; }
